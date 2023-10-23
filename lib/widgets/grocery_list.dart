a2fb10d19230573e2cb7aad665a50acc9a5e102d
SHIVAM <shivampatel0700@gmail.com>
2023-10-23 15:05:27 +0530

    Post implemented.

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -18,7 +18,7 @@ class _GroceryListState extends State<GroceryList> {
         builder: (context) => const NewItem(),
       ),
     );
-    if(newItem == null){
+    if (newItem == null) {
       return;
     }
     setState(() {
@@ -28,32 +28,50 @@ class _GroceryListState extends State<GroceryList> {
 
   @override
   Widget build(BuildContext context) {
+    Widget content = const Center(
+      child: Text('No items added yet.'),
+    );
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (context, index) {
+          return Dismissible(
+            key: ValueKey(_groceryItems[index].id),
+            onDismissed: (direction) {
+              // Remove the item from the data source.
+              setState(() {
+                _groceryItems.removeAt(index);
+              });
+            },
+            background: Container(
+              color: Colors.red,
+            ),
+            child: ListTile(
+              leading: Container(
+                height: 20,
+                width: 20,
+                color: _groceryItems[index].category.color,
+              ),
+              title: Text(_groceryItems[index].name),
+              trailing: Text(
+                '${_groceryItems[index].quantity}',
+                style: const TextStyle(fontSize: 18),
+              ),
+            ),
+          );
+        },
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
         actions: [
-          IconButton(
-              onPressed: _addNewItem,
-              icon: const Icon(Icons.add))
+          IconButton(onPressed: _addNewItem, icon: const Icon(Icons.add))
         ],
       ),
-      body: ListView.builder(
-        itemCount: _groceryItems.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            leading: Container(
-              height: 20,
-              width: 20,
-              color: _groceryItems[index].category.color,
-            ),
-            title: Text(_groceryItems[index].name),
-            trailing: Text(
-              '${_groceryItems[index].quantity}',
-              style: const TextStyle(fontSize: 18),
-            ),
-          );
-        },
-      ),
+      body: content,
     );
   }
 }
--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,7 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:shopping_list/data/categoriesData.dart';
 import 'package:shopping_list/models/categoriesModel.dart';
-import 'package:shopping_list/models/groceryItemsModel.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -18,17 +19,28 @@ class _NewItemState extends State<NewItem> {
     var _enteredQuantity = 1;
     var _selectedCategory = categories[Categories.vegetables]!;
 
-    void _submitItem() {
+    void _submitItem() async {
       if (_formKey.currentState!.validate()) {
         _formKey.currentState!.save();
-        Navigator.of(context).pop(
-          GroceryItemsModel(
-            id: DateTime.now().toString(),
-            name: _enteredName,
-            quantity: _enteredQuantity,
-            category: _selectedCategory,
-          ),
+        final url = Uri.https('flutter-prep-9863a-default-rtdb.firebaseio.com',
+            'shopping-list.json');
+        final response = await http.post(
+          url,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: json.encode({
+            'name': _enteredName,
+            'quantity': _enteredQuantity,
+            'category': _selectedCategory.title,
+          }),
         );
+        print(response.body);
+        print(response.statusCode);
+        if (!context.mounted) {
+          return;
+        }
+        Navigator.of(context).pop();
       }
     }
 
